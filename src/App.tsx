df45e460053424c213909e03eff9eb407b8de0a7
thsmale <60244532+thsmale@users.noreply.github.com>
2026-03-11 01:02:12 -0700

    basic toggle group with countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,84 @@
-import { Box, Text } from 'grommet';
+import { useState } from 'react';
+import {
+  Box,
+  Data,
+  DataFilters,
+  DataSearch,
+  DataSummary,
+  DataTable,
+  Grommet,
+  Heading,
+  Text,
+  ToggleGroup,
+  Toolbar,
+} from 'grommet';
+import {
+  AppsRounded,
+  List as ListIcon,
+  Table,
+} from 'grommet-icons';
+import { CardView } from './CardView';
+import { miracles } from './data/miracles';
+
+const toggleOptions = [
+  {
+    icon: <Table a11yTitle='Table view' />,
+    value: 'table',
+    tip: 'Table',
+  },
+  {
+    icon: <ListIcon a11yTitle='List view' />,
+    value: 'list',
+    tip: 'List',
+  },
+  {
+    icon: <AppsRounded a11yTitle='Card view' />,
+    value: 'card',
+    tip: 'Card',
+  },
+]
 
 function App() {
+  const [value, setValue] = useState('table');
   return (
-    <Box>
-      <Text>Hello world!</Text>
-    </Box>
+    <Grommet>
+      <Box gap='medium'>
+        <Heading>Miracles List</Heading>
+        <Data
+          data={miracles}
+          properties={{
+            country: { label: 'Country' }
+          }}
+        >
+          <Toolbar>
+            <DataSearch />
+            <DataFilters layer />
+            <ToggleGroup
+              onToggle={e => {
+                if (e.value.length) setValue(e.value);
+              }}
+              value={value}
+              options={toggleOptions}
+            />
+          </Toolbar>
+          <DataSummary />
+          { value === 'table' && (
+            <DataTable
+              aria-describedby='eucharistic-miracles-list'
+              columns={[
+                {
+                    property: 'country',
+                    header: <Text>Country</Text>,
+                    primary: true,
+                }
+              ]}
+              verticalAlign={{ body : 'top' }}
+            />
+          )}
+          { value === 'card' && <CardView /> }
+        </Data>
+      </Box>
+    </Grommet>
   )
 }
 
---
+++ src/CardView.tsx
@@ -0,0 +1,41 @@
+import {
+    Anchor,
+    Box,
+    Cards,
+    Card as GrommetCard,
+    CardBody,
+    Text,
+    Heading,
+} from 'grommet';
+import { miracles } from './data/miracles';
+
+export const CardView = () => {
+    return (
+        <Cards size='medium' margin={{ bottom: 'medium' }}>
+            {datum => (
+                <Card
+                    key={datum.country}
+                    country={datum.country}
+                    description='Lorem Ipsum'
+                    src='http://localhost'
+                />
+            )}
+        </Cards>
+    )
+}
+
+const Card = ({ country, description, src }) => {
+    return (
+        <GrommetCard elevation='medium'>
+            <CardBody gap='medium'>
+                <Box gap='3xsmall'>
+                    <Heading level={2} margin='none'>
+                        {country}
+                    </Heading>
+                    <Text>{description}</Text>
+                </Box>
+                <Anchor label='View details' />
+            </CardBody>
+        </GrommetCard>
+    )
+}
