30afee4374fe6c15b8b8c1ea33f0076502ca92c1
thsmale <60244532+thsmale@users.noreply.github.com>
2026-03-11 01:24:21 -0700

    add country data
    in middle of adding city, year

---
+++ src/data/miracles.ts
@@ -0,0 +1,82 @@
+/*
+ 01_ARGENTINE   10_INDIA        19_SWITZERLAND
+ 02_AUSTRIA     11_ITALY        20_VENEZUELA
+ 03_BELGIUM     12_MARTINIQUE  '21_SAINTS, MYSTICS AND THE EUCHARIST'
+ 04_COLOMBIA    13_MEXICO      '22_OUR LADY AND THE EUCHARIST'
+ 05_CROATIA     14_PERU        '23_MIRACULOUS COMMUNIONS'
+ 06_EGYPT       15_POLAND      '24_MAPS OF COUNTRIES'
+ 07_FRANCE      16_PORTUGAL    '27_PRESENTATION OF EXHIBITION'
+ 08_GERMANY     17_REUNION
+ 09_HOLLAND     18_SPAIN
+ */
+
+const miracles = [
+  {
+    country: 'Argentina',
+    city: 'Buenos Aires',
+    year: '1992-1994-1996',
+  },
+  {
+    country: 'Austria',
+    city: 'Fiecht', 
+    year: '1310',
+  },
+  {
+    country: 'Belgium',
+  },
+  {
+    country: 'Colombia',
+  },
+  {
+    country: 'Croatia',
+  },
+  {
+    country: 'Egypt',
+  },
+  {
+    country: 'France',
+  },
+  {
+    country: 'Germany',
+  },
+  {
+    country: 'Holland',
+  },
+  {
+    country: 'India',
+  },
+  {
+    country: 'Italy',
+  },
+  {
+    country: 'Martinique',
+  },
+  {
+    country: 'Mexico',
+  },
+  {
+    country: 'Peru',
+  },
+  {
+    country: 'Poland',
+  },
+  {
+    country: 'Portugal',
+  },
+  {
+    country: 'Reunion',
+  },
+  {
+    country: 'Spain',
+  },
+  {
+    country: 'Switzerland',
+  },
+  {
+    country: 'Venezuela',
+  },
+]
+
+export {
+  miracles,
+}
