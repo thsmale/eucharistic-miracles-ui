4b884073c38a8afe45923c6795f1860b9b065b4b
thsmale <60244532+thsmale@users.noreply.github.com>
2026-04-21 18:58:56 -0700

    add icon aka logo to header

--- src/Header.tsx
+++ src/Header.tsx
@@ -16,14 +16,26 @@ export const AppHeader = () => {
       <Button href='/'>
         <Box
           direction='row'
-          align='start'
-          gap='medium'
+          align='center'
+          gap='xsmall'
           pad={{ vertical: '3xsmall' }}
           responsive={false}
         >
+          <svg width="32" height="32" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g fill="black">
+              <path d="M 16 22 C 16 38 48 38 48 22 Z" />
+              <circle cx="32" cy="42" r="4" />
+              <path d="M 28 50 Q 32 34 36 50 Z" />
+              <ellipse cx="32" cy="52" rx="20" ry="4" />
+            </g>
+            <g>
+              <circle cx="32" cy="26" r="14" fill="white" stroke="black" stroke-width="2" />
+              <path d="M 32 16 V 36 M 22 26 H 42" stroke="black" stroke-width="2.5" stroke-linecap="round" />
+            </g>
+          </svg>
           <Box direction='row' gap='xsmall' wrap>
             <Text color='text-strong' weight='bold'>
-                            Carlo Acutis
+             Carlo Acutis
             </Text>
             <Text color='text-strong'>Miracles List</Text>
           </Box>
