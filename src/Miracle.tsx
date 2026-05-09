386740a7dd68f18d27a39ea38626da4fd77afc4c
thsmale <60244532+thsmale@users.noreply.github.com>
2026-05-08 22:22:30 -0700

    add copyright

--- src/Miracle.tsx
+++ src/Miracle.tsx
@@ -4,6 +4,7 @@ import {
   Box,
   Button,
   Carousel,
+  Footer,
   Heading,
   Image,
   Page,
@@ -53,69 +54,74 @@ export const Miracle = () => {
   ].join('%0A%0A')
 
   return (
-    <Page background='background-front' kind='narrow' pad={{ bottom: 'xlarge' }}>
-      <PageContent gap='medium'>
-        <PageHeader
-          title={`${miracle?.title} ${miracle?.city}`}
-          subtitle={(
-            <Box gap='small'>
-              <Text>
-                {miracle?.country} {miracle.year}
-              </Text>
-              <Box direction='row' gap='xsmall'>
-                <Button
-                  hoverIndicator
-                  icon={<X />}
-                  href={`https://twitter.com/intent/tweet?text=${tweetContent}`}
-                  target="_blank"
-                />
-                <Button
-                  hoverIndicator
-                  icon={<Facebook />}
-                  href={`http://www.facebook.com/sharer.php?u=${window.location.href}`}
-                  target="_blank"
-                />
-                <Button
-                  hoverIndicator
-                  icon={<Link />}
-                  onClick={() => navigator.clipboard.writeText(window.location.href)}
-                />
-                <Button
-                  hoverIndicator
-                  icon={<DocumentPdf />}
-                  href={miracle.pdfLink}
-                  target="_blank"
-                />
+    <Box>
+      <Page background='background-front' kind='narrow' pad={{ bottom: 'xlarge' }}>
+        <PageContent gap='medium'>
+          <PageHeader
+            title={`${miracle?.title} ${miracle?.city}`}
+            subtitle={(
+              <Box gap='small'>
+                <Text>
+                  {miracle?.country} {miracle.year}
+                </Text>
+                <Box direction='row' gap='xsmall'>
+                  <Button
+                    hoverIndicator
+                    icon={<X />}
+                    href={`https://twitter.com/intent/tweet?text=${tweetContent}`}
+                    target="_blank"
+                  />
+                  <Button
+                    hoverIndicator
+                    icon={<Facebook />}
+                    href={`http://www.facebook.com/sharer.php?u=${window.location.href}`}
+                    target="_blank"
+                  />
+                  <Button
+                    hoverIndicator
+                    icon={<Link />}
+                    onClick={() => navigator.clipboard.writeText(window.location.href)}
+                  />
+                  <Button
+                    hoverIndicator
+                    icon={<DocumentPdf />}
+                    href={miracle.pdfLink}
+                    target="_blank"
+                  />
+                </Box>
+              </Box>
+            )}
+          />
+          <Box>
+            <Heading margin='none' level={3}>Introduction</Heading>
+            <Paragraph margin='none' fill={true}>{miracle?.intro}</Paragraph>
+          </Box>
+          <Box>
+            <Heading margin='none' level={3}>Overview</Heading>
+            <Paragraph margin='none' fill={true}>
+              {parse(miracle?.overview)}
+            </Paragraph>
+          </Box>
+          {miracle?.images.length > 0 && (
+            <Box>
+              <Heading margin='none' level={3}>Images</Heading>
+              <Box width="medium" height="medium" overflow="hidden">
+                <Carousel fill controls="arrows">
+                  {miracle.images.map(img => (
+                    <Box key={img.path} width="medium" height="medium">
+                      <Image fit="cover" src={`/images/${img.path}`} />
+                      <Text size="small">{parse(img.caption)}</Text>
+                    </Box>
+                  ))}
+                </Carousel>
               </Box>
             </Box>
           )}
-        />
-        <Box>
-          <Heading margin='none' level={3}>Introduction</Heading>
-          <Paragraph margin='none' fill={true}>{miracle?.intro}</Paragraph>
-        </Box>
-        <Box>
-          <Heading margin='none' level={3}>Overview</Heading>
-          <Paragraph margin='none' fill={true}>
-            {parse(miracle?.overview)}
-          </Paragraph>
-        </Box>
-        {miracle?.images.length > 0 && (
-          <Box>
-            <Heading margin='none' level={3}>Images</Heading>
-            <Box width="medium" height="medium" overflow="hidden">
-              <Carousel fill controls="arrows">
-                {miracle.images.map(img => (
-                  <Box key={img.path} width="medium" height="medium">
-                    <Image fit="cover" src={`/images/${img.path}`} />
-                    <Text size="small">{parse(img.caption)}</Text>
-                  </Box>
-                ))}
-              </Carousel>
-            </Box>
-          </Box>
-        )}
-      </PageContent>
-    </Page>
+        </PageContent>
+      </Page>
+      <Footer border={{ side: 'top' }} pad='medium'>
+        <Text size='small'>{miracle.copyright}</Text>
+      </Footer>
+    </Box>
   )
 }
