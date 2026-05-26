8f03b73ac7da335c68ecf09a8d7c90638ea18364
thsmale <60244532+thsmale@users.noreply.github.com>
2026-05-26 03:24:38 -0700

    rm debugs

--- worker/index.ts
+++ worker/index.ts
@@ -8,20 +8,10 @@ export default {
   async fetch(request, environment): Promise<Response> {
     const { pathname }= new URL(request.url);
     if (pathname.startsWith("/json/") || pathname.startsWith("/images/")) {
-      try {
-        const url = new URL(pathname, environment.CDN_URL);
-        const response = await fetch(url, {
-          headers: { 'x-auth': environment.AUTH_SECRET }
-        });
-        
-        if (!response.ok) {
-          return new Response(`Worker failed with ${response.status}`, { status: 503 });
-        }
-
-        return response;
-      } catch (err: any) {
-        return new Response(`Worker response: ${err?.message}`, { status: 503 })
-      }
+      const url = new URL(pathname, environment.CDN_URL);
+      return await fetch(url, {
+        headers: { 'x-auth': environment.AUTH_SECRET }
+      });
     }
     return new Response(null, { status: 404 });
   }
