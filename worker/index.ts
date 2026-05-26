36e0c77e3cdfc1cc4e10ff36ccc9b5e6bcc74fb8
thsmale <60244532+thsmale@users.noreply.github.com>
2026-05-26 01:30:06 -0700

    add pathname.startsWith

--- worker/index.ts
+++ worker/index.ts
@@ -6,6 +6,9 @@ export default {
   async fetch(request, environment): Promise<Response> {
     const url = new URL(request.url);
     console.log({ url });
-    return await environment.MY_CERT.fetch(url)
+    if (url.pathname.startsWith("/json/") || url.pathname.startsWith("/images/")) {
+      return await environment.MY_CERT.fetch(url)
+    }
+    return new Response(null, { status: 404 });
   }
 } satisfies ExportedHandler<Env>;
