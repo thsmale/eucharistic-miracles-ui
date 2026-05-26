f059c1e3d744e05f44a131de7ee8d18acd8508df
thsmale <60244532+thsmale@users.noreply.github.com>
2026-05-26 02:02:02 -0700

    add detailed response
    cause I cant see any logs

--- worker/index.ts
+++ worker/index.ts
@@ -7,8 +7,18 @@ export default {
   async fetch(request, environment): Promise<Response> {
     const { pathname }= new URL(request.url);
     if (pathname.startsWith("/json/") || pathname.startsWith("/images/")) {
-      const url = new URL(pathname, environment.CDN_URL);
-      return await environment.MY_CERT.fetch(url)
+      try {
+        const url = new URL(pathname, environment.CDN_URL);
+        const response = await environment.MY_CERT.fetch(url)
+        
+        if (!response.ok) {
+          return new Response(`Worker failed with ${response.status}`, { status: 503 });
+        }
+
+        return response;
+      } catch (err: any) {
+        return new Response(`Worker response: ${err?.message}`, { status: 503 })
+      }
     }
     return new Response(null, { status: 404 });
   }
