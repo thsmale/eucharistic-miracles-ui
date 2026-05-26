8ff08d99d7631582089a3b6f3d3c0cec8c184608
thsmale <60244532+thsmale@users.noreply.github.com>
2026-05-25 23:55:38 -0700

    add log to worker to see if running

--- worker/index.ts
+++ worker/index.ts
@@ -4,6 +4,7 @@ interface Env {
 
 export default {
   async fetch(request, environment): Promise<Response> {
+    console.log('hello from worker');
     const url = new URL(request.url);
     return await environment.MY_CERT.fetch(url)
   }
