5edc84105387ff544ed0bfd6bebd7081af86ec04
thsmale <60244532+thsmale@users.noreply.github.com>
2026-05-26 01:48:59 -0700

    redirect requests to CDN
    receiving 1019 since request sent again to same url perhaps

--- worker/index.ts
+++ worker/index.ts
@@ -1,13 +1,13 @@
 interface Env {
   MY_CERT: Fetcher;
+  CDN_URL: string,
 }
 
 export default {
   async fetch(request, environment): Promise<Response> {
-    const url = new URL(request.url);
-    console.log({ url });
-    if (url.pathname.startsWith("/json/") || url.pathname.startsWith("/images/")) {
-      return new Response(JSON.stringify({ 'hello': 'world' }), { status: 404 });
+    const { pathname }= new URL(request.url);
+    if (pathname.startsWith("/json/") || pathname.startsWith("/images/")) {
+      const url = new URL(pathname, environment.CDN_URL);
       return await environment.MY_CERT.fetch(url)
     }
     return new Response(null, { status: 404 });
