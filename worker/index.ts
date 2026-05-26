bb6aa66fc026c085d6c96a01d10282207a192f1e
thsmale <60244532+thsmale@users.noreply.github.com>
2026-05-26 00:10:30 -0700

    add fake response

--- worker/index.ts
+++ worker/index.ts
@@ -5,6 +5,9 @@ interface Env {
 export default {
   async fetch(request, environment): Promise<Response> {
     console.log('hello from worker');
+    return Response.json({
+      hello: "world",
+    })
     const url = new URL(request.url);
     return await environment.MY_CERT.fetch(url)
   }
